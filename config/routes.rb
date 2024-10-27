b229304107aea031111063194c706d3bcc11ca44
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-10-27 23:26:00 +0000

    final touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "zebra", :action => "home" })
 
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "zebra", :action => "dice" })
+
+  get("/dice/2/6", { :controller => "zebra", :action => "dice" })
 end
